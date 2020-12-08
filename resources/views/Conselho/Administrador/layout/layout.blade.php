b781ce6c359edee3980ef8d78701eade869513d7
noytroner <darinformax@gmail.com>
2020-12-07 21:55:03 -0300

    Azul-conselho

---
+++ resources/views/Conselho/Administrador/layout/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('conselho/css/style.css') }}">
+</head>
+<body>
+    <header>
+        <div class="col-12 flex-start bg-azul-conselho">
+            <h1 class=" text-uppercase">CONseLHO DE CLASSE</h1>
+            <i></i>
+        </div>
+    </header>
+
+    <script src="{{ asset('conselho/js/jquery.js') }}"></script>
+    <script src="{{ asset('conselho/js/bootstrap.js') }}"></script>
+</body>
+</html>
