dd3f2ed796da87ee7bc3443506551bd7ea44297b
noytroner <darinformax@gmail.com>
2021-01-23 11:06:38 -0300

    pull

---
+++ resources/views/Conselho/Administrador/Disciplinas/disciplinas/disciplinas.blade.php
@@ -0,0 +1,86 @@
+@extends('Conselho/Administrador/layout/layout')
+
+@section('main-page')
+    <div class="p-tipos-atendimentos">
+        <div>
+            <div>
+                <h1 class="titulo-pagina">Disciplinas</h1>
+            </div>
+        </div>
+        <div>
+            <div class=" d-flex justify-content-between font-size-756">
+                <div>
+                    <p>
+                        Mostrar:
+                        <select class="Selecao" name="numeroDeRegistros" id="numeroDeRegistros">
+                            <option value="10">10 registros</option>
+                            <option value="25">25 registros</option>
+                            <option value="40">40 registros</option>
+                        </select>
+                    </p>
+                </div>
+                <div>
+                    <p class=" text-right">
+                        Buscar:
+                        <input class="Buscador" type="text">
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="tabela-tipos-atendimentos font-size-756">
+            <div class="grid-tabela-disciplinas border-bottom-conselho">
+                <div>
+                    <p class="m-0 font-weight-bold">Nome das disciplinas</p>
+                </div>
+                <div class="text-center">
+                    <p class="m-0 font-weight-bold">Categoria da disciplina</p>
+                </div>
+                <div class="text-center">
+                    <p class="m-0 font-weight-bold">Editar</p>
+                </div>
+                <div class="text-center">
+                    <p class="m-0 font-weight-bold">Excluir</p>
+                </div>
+            </div>
+            <div class="grid-tabela-disciplinas border-bottom-conselho">
+                <div>
+                    <p class="item-tipo-atendimento font-size-542">Redes de computadores</p>
+                </div>
+                <div>
+                    <p class="item-tipo-atendimento text-center font-size-542">Técnica</p>
+                </div>
+                <div class="text-center">
+                    <a class="botao-editar" href=""><i class="fas fa-pencil-alt"></i></a>
+                </div>
+                <div class="text-center">
+                    <a class="botao-excluir" href=""><i class="fas fa-trash-alt"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class=" mb-3 font-size-542">
+            Mostrando...
+        </div>
+        <div class=" d-flex font-size-658">
+            <div>
+                <a class="botao-adicionar-tipo-atendimento" href="">Adicionar</a>
+            </div>
+            <div class="controle-de-paginas">
+                <div>
+                    <a href="">Primeiro</a>
+                </div>
+                <div>
+                    <a href="">Anterior</a>
+                </div>
+                <div>
+                    <span>1</span>
+                </div>
+                <div>
+                    <a href="">Próximo</a>
+                </div>
+                <div>
+                    <a href="">Último</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,19 @@ Route::get('/teste', function(){
 Route::get('/teste2', function(){
     return view("Conselho.Administrador.Atendimentos.criar&editar_atendimentos.criar&editar");
 });
+
+Route::get('/teste3', function(){
+    return view("Conselho.Administrador.Advertencias.criar&editar_advertencias.criar&editar");
+});
+
+Route::get('/teste4', function(){
+    return view("Conselho.Administrador.Advertencias.tipos_de_advertencia.tipos");
+});
+
+Route::get('/teste5', function(){
+    return view("Conselho.Administrador.Disciplinas.disciplinas.disciplinas");
+});
+
+Route::get('/teste6', function(){
+    return view("Conselho.Administrador.Disciplinas.criar&editar_disciplina.criar&editar");
+});
